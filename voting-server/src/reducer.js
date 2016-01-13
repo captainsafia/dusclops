9eb5c62ec78a4575e31835ea13dc20f46de6cca3
Safia Abdalla <safia@safia.rocks>
2016-01-13 14:25:16 -0600

    Add code for reducer

--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -1,4 +1,15 @@
+import {setEntries, next, vote} from './core';
+
 export default function reducer(state, action) {
     // A reducer executes the function described by the action object on
     // the state that is passed in
+    switch (action.type) {
+        case 'SET_ENTRIES':
+            return setEntries(state, action.entries);
+        case 'NEXT':
+            return next(state);
+        case 'VOTE':
+            return vote(state, action.entry);
+    }
+    return state;
 }
