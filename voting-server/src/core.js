78789e7b62e11c1e242037481b03dc03185eca93
Safia Abdalla <safia@safia.rocks>
2016-01-12 19:10:58 -0600

    Add vote results back to entries

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -4,8 +4,26 @@ export function setEntries(state, entries) {
     return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+    if (!vote) {
+        return [];
+    }
+
+    const [movieA, movieB] = vote.get('pair');
+    const movieAVotes = vote.getIn(['tally', movieA], 0);
+    const movieBVotes = vote.getIn(['tally', movieB], 0);
+
+    if (movieAVotes > movieBVotes) {
+        return [movieA];
+    } else if (movieAVotes < movieBVotes) {
+        return [movieB];
+    } else {
+        return [movieA, movieB];
+    }
+}
+
 export function next(state) {
-    const entries = state.get('entries');
+    const entries = state.get('entries').concat(getWinners(state.get('vote')));
     return state.merge({
         vote: Map({pair: entries.take(2)}),
         entries: entries.skip(2)
