bf7ff20f815ea831a891356c0820e4cb1a246917
Safia Abdalla <safia@safia.rocks>
2016-01-18 15:20:00 -0600

    Updated webpack configuration

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: [
         'webpack-dev-server/client?http://localhost:8080',
         'webpack/hot/only-dev-server',
-        './src/index.js',
+        './src/index.jsx',
     ],
     module: {
         loaders: [{
@@ -12,6 +12,9 @@ module.exports = {
             exclude: /node_modules/,
             loader: 'react-hot!babel'
         }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
     output: {
         path: __dirname + '/dist',
